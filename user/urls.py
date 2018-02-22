1ed2a074f32032bf7022bf69dec41c77bcf28bd7
VladimirBurash <vladimirburash@ktsstudio.ru>
2018-02-22 03:44:39 +0300

    fix

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 
 from django.conf.urls import url
 
-from user.views import LoginView, GetView, RegisterView, LogoutView, VKView, VKAuthView, FBView
+from user.views import LoginView, GetView, RegisterView, LogoutView, VKView, VKAuthView, FBView, FBAuthView
 
 urlpatterns = [
     url(r'^login', LoginView.as_view()),
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^register', RegisterView.as_view()),
     url(r'^logout', LogoutView.as_view()),
     url(r'^vk-auth', VKAuthView.as_view()),
-    url(r'^fb-auth', VKAuthView.as_view()),
+    url(r'^fb-auth', FBAuthView.as_view()),
     url(r'^vk', VKView.as_view()),
     url(r'^fb', FBView.as_view()),
 
