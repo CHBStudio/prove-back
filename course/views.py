3cdd4e2ea2ccbe9199d75382eeea9c137b273cb4
VladimirBurash <vladimirburash@ktsstudio.ru>
2018-02-23 02:51:53 +0300

    fix

--- course/views.py
+++ course/views.py
@@ -47,7 +47,8 @@ class GetView(NoCSRFView):
             raise PermissionDenied
         resp = []
         schedules = Schedule.objects.filter(course_id=course.id)
-        weeks = set([sc.week for sc in schedules])
+        ws = [sc.week for sc in schedules]
+        weeks = set(ws.sort())
         for week in weeks:
             weeklist = []
             schedules = Schedule.objects.filter(course_id=course_id,week=week)
